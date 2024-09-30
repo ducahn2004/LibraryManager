5691031d96bffcefa274b980c4b194e2504bfddd
ducahn2004 <nducanh180304@gmail.com>
2024-09-30 17:30:55 +0700

    Update initial Java folder
    Update initial Java folder 1730 3009

---
+++ LibraryManagerCode/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.group4.librarymanagercode {
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.web;
+
+  requires org.controlsfx.controls;
+  requires com.dlsc.formsfx;
+  requires net.synedra.validatorfx;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.bootstrapfx.core;
+  requires eu.hansolo.tilesfx;
+  requires com.almasb.fxgl.all;
+
+  opens org.group4.librarymanagercode to javafx.fxml;
+  exports org.group4.librarymanagercode;
+}
