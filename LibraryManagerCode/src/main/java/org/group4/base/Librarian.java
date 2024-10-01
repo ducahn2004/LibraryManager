ce4c8c6e5c1b537a463e66a24466bda89faf5069
TuanAnhSatoh <tuananhdx9a@gmail.com>
2024-10-01 20:16:22 +0700

    Delete compiled classes and FXML resources
    Removed all compiled class files, FXML files, and image assets from the target directory to clean up the project. These generated files are unnecessary for version control and can be rebuilt.

--- LibraryManagerCode/src/main/java/org/group4/base/Librarian.java
+++ LibraryManagerCode/src/main/java/org/group4/base/Librarian.java
@@ -2,7 +2,12 @@ package org.group4.base;
 
 import java.lang.reflect.Member;
 
-public class Librarian {
+public class Librarian extends Account {
+
+  public Librarian(String id, String password, AccountStatus status, Person person) {
+    super(id, password, status, person);
+  }
+
   public boolean addBookItem(BookItem bookItem) {
     return true;
   }
