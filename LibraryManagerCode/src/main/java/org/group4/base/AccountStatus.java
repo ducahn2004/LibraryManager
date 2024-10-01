9eda0827eca25c23c2155a806fa9e19ccf84462a
TuanAnhSatoh <tuananhdx9a@gmail.com>
2024-10-01 19:54:12 +0700

    Enhance Account and Member classes with constructors and setters
    Updated the `Account` class to include constructors, getters, and setters. Improved the `Member` class by extending `Account` and adding initialization in the constructor. Additionally, updated `AccountStatus` enum to use camelCase for consistency.

--- LibraryManagerCode/src/main/java/org/group4/base/AccountStatus.java
+++ LibraryManagerCode/src/main/java/org/group4/base/AccountStatus.java
@@ -1,10 +1,9 @@
 package org.group4.base;
 
 public enum AccountStatus {
-  ACTIVE,
-  CLOSED,
-  CANCELED,
-  BLACKLISTED,
-  BLOCKED,
-  NONE
+  Active,
+  Closed,
+  Canceled,
+  Blacklisted,
+  None
 }
--- LibraryManagerCode/src/main/java/org/group4/base/Member.java
+++ LibraryManagerCode/src/main/java/org/group4/base/Member.java
@@ -2,11 +2,34 @@ package org.group4.base;
 
 import java.time.LocalDate;
 
-public class Member {
+public class Member extends Account{
   private LocalDate dateOfMembership;
   private int totalBooksCheckedOut;
+
+  public Member() {
+    super(null, null, AccountStatus.None, null);
+    this.dateOfMembership = dateOfMembership;
+    this.totalBooksCheckedOut = totalBooksCheckedOut;
+  }
+
+  public LocalDate getDateOfMembership() {
+    return dateOfMembership;
+  }
+
+  public void setDateOfMembership(LocalDate dateOfMembership) {
+    this.dateOfMembership = dateOfMembership;
+  }
 
   public int getTotalBooksCheckedOut() {
     return totalBooksCheckedOut;
   }
+
+  public void setTotalBooksCheckedOut(int totalBooksCheckedOut) {
+    this.totalBooksCheckedOut = totalBooksCheckedOut;
+  }
+
+  public int getTotalCheckedoutBooks() {
+    return totalBooksCheckedOut;
+  }
+
 }
