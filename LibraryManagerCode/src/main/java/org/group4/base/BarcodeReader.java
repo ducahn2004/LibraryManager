9f61e6074e37f1001187a1191e02fae22874f18a
TuanAnhSatoh <tuananhdx9a@gmail.com>
2024-10-01 07:13:21 +0700

    Add BookStatus enum and BarcodeReader class
    Introduced BookStatus enum and BarcodeReader class to the base package. These changes include fields and a method skeleton for BarcodeReader to facilitate future development. This prepares the codebase for handling book status and barcode reading functionalities.

---
+++ LibraryManagerCode/src/main/java/org/group4/base/BarcodeReader.java
@@ -0,0 +1,14 @@
+package org.group4.base;
+
+import java.time.LocalDate;
+
+public class BarcodeReader {
+  private String id;
+  private LocalDate registereAt;
+  private boolean active;
+
+  public boolean isActive() {
+
+  }
+
+}
