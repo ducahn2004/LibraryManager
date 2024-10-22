91f72796f540ac56e11a46665dd6f7f3f344b915
ducahn2004 <nducanh180304@gmail.com>
2024-10-22 16:01:39 +0700

    update BookView.fxml StudentView.fxml StudentViewController

--- LibraryManagerCode/src/main/java/org/group4/librarymanagercode/Admin/StudentViewController.java
+++ LibraryManagerCode/src/main/java/org/group4/librarymanagercode/Admin/StudentViewController.java
@@ -1,4 +1,42 @@
 package org.group4.librarymanagercode.Admin;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class StudentViewController {
+
+  public JFXButton homeButton;
+  public JFXButton studentsButton;
+  public JFXButton bookButton;
+  public JFXButton addBookButton;
+  public JFXButton returnBookButton;
+  public JFXButton settingButton;
+  public JFXButton closeButton;
+
+  public void deleteStudentRecord(ActionEvent actionEvent) {
+  }
+
+  public void requestMenu(ContextMenuEvent contextMenuEvent) {
+  }
+
+  public void fetchStudentWithKey(KeyEvent keyEvent) {
+  }
+
+  public void fetchStudentFeesDetails(MouseEvent mouseEvent) {
+  }
+
+  public void updateStudent(ActionEvent actionEvent) {
+  }
+
+  public void deleteStudent(ActionEvent actionEvent) {
+  }
+
+  public void saveStudent(ActionEvent actionEvent) {
+  }
+
+  public void cancel(ActionEvent actionEvent) {
+  }
 }
