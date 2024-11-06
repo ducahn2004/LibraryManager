7aae8ec9a39452efeb36e564cab9af2be0407043
ducahn2004 <nducanh180304@gmail.com>
2024-11-06 08:13:35 +0700

    Update BookView, MemberView.fxml, MemberViewController

--- LibraryManagerCode/src/main/java/org/group4/database/MemberDatabase.java
+++ LibraryManagerCode/src/main/java/org/group4/database/MemberDatabase.java
@@ -7,8 +7,20 @@ public class MemberDatabase extends Database<Member> {
     private static final MemberDatabase instance = new MemberDatabase();
 
     private MemberDatabase() {
-        Member member = new Member("member", "123456", new Person("Member", "Member@gmail.com", "012345689"));
+        Member member = new Member("22022168", "22022168", new Person("Alice", "22022168@vnu.edu.vn", "012345689"));
         addItem(member);
+        Member member1 = new Member("22022181", "22022181", new Person("Bob", "22022181@vnu.edu.vn", "012345689"));
+        addItem(member1);
+        Member member2= new Member("22022121", "22022121", new Person("Candy", "22022121@vnu.edu.vn", "012345689"));
+        addItem(member2);
+        Member member3 = new Member("22022102", "22022102", new Person("Dave", "22022102@vnu.edu.vn", "012345689"));
+        addItem(member3);
+        Member member4 = new Member("22022190", "22022190", new Person("Evan", "22022190@vnu.edu.vn", "012345689"));
+        addItem(member4);
+        Member member5 = new Member("22022189", "22022189", new Person("Frank", "22022189@vnu.edu.vn", "012345689"));
+        addItem(member5);
+        Member member6 = new Member("22022167", "22022167", new Person("Hold", "22022167@vnu.edu.vn", "012345689"));
+        addItem(member6);
     }
     public static MemberDatabase getInstance() {
         return instance;
--- LibraryManagerCode/src/main/java/org/group4/librarymanagercode/MemberViewController.java
+++ LibraryManagerCode/src/main/java/org/group4/librarymanagercode/MemberViewController.java
@@ -1,52 +1,122 @@
 package org.group4.librarymanagercode;
 
 import com.jfoenix.controls.JFXButton;
+import java.util.Arrays;
+import java.util.List;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import org.group4.base.entities.Person;
+import org.group4.base.users.Member;
+import org.group4.database.MemberDatabase;
 
 public class MemberViewController {
 
-  public JFXButton homeButton;
-  public JFXButton studentsButton;
-  public JFXButton bookButton;
-  public JFXButton addBookButton;
-  public JFXButton returnBookButton;
-  public JFXButton settingButton;
-  public JFXButton closeButton;
-  public ContextMenu selectStudentContext;
-  public MenuItem selectMenu;
-  public TableColumn check;
-  public TableColumn memberTableID;
-  public TableColumn memberTableName;
-  public TableColumn memberTablePhone;
-  public TableColumn memberTableEmail;
-  public TableView memberTable;
-  public JFXButton delete;
-  public JFXButton update;
-  public JFXButton save;
-  public JFXButton cancel;
-  public TextField memberPhone;
-  public TextField memberEmail;
-  public TextField memberName;
-  public TextField memberID;
+  @FXML
+  private TextField searchField;
+  private ObservableList<Member> memberList = FXCollections.observableArrayList();
+  @FXML
+  private JFXButton homeButton;
+  @FXML
+  private JFXButton bookButton;
+  @FXML
+  private JFXButton returnBookButton;
+  @FXML
+  private JFXButton settingButton;
+  @FXML
+  private JFXButton closeButton;
+  @FXML
+  private ContextMenu selectMemberContext;
+  @FXML
+  private MenuItem selectMenu;
+  @FXML
+  private TableColumn<Member, String> memberTableID;
+  @FXML
+  private TableColumn<Member, String> memberTableName;
+  @FXML
+  private TableColumn<Member, String> memberTablePhone;
+  @FXML
+  private TableColumn<Member, String> memberTableEmail;
+  @FXML
+  private TableView<Member> memberTable;
+  @FXML
+  private JFXButton delete;
+  @FXML
+  private JFXButton update;
+  @FXML
+  private JFXButton save;
+  @FXML
+  private JFXButton cancel;
+  @FXML
+  private TextField memberPhone;
+  @FXML
+  private TextField memberEmail;
+  @FXML
+  private TextField memberName;
+  @FXML
+  private TextField memberID;
 
-  public void deleteStudentRecord(ActionEvent actionEvent) {
+
+  @FXML
+  public void initialize() {
+    // Set up table columns
+    memberTableID.setCellValueFactory(
+        cellData -> new SimpleStringProperty(cellData.getValue().getId()));
+    memberTableName.setCellValueFactory(
+        cellData -> new SimpleStringProperty(cellData.getValue().getPerson().getName()));
+    memberTablePhone.setCellValueFactory(
+        cellData -> new SimpleStringProperty(cellData.getValue().getPerson().getPhoneNumber()));
+    memberTableEmail.setCellValueFactory(
+        cellData -> new SimpleStringProperty(cellData.getValue().getPerson().getEmail()));
+    // Load member data
+    loadMembers();
+    memberTable.setItems(memberList);
+
+    // Set up row selection listener
+    memberTable.getSelectionModel().selectedItemProperty()
+        .addListener((observable, oldValue, newValue) -> {
+          if (newValue != null) {
+            displayMemberDetails((Member) newValue);
+          }
+        });
+  }
+
+  private void loadMembers() {
+    // Load members from a data source (e.g., database or hardcoded list)
+    // Example hardcoded members:
+    memberList.clear();
+    memberList.addAll(MemberDatabase.getInstance().getItems());
+    System.out.println("Loaded members Done ");
+  }
+
+  private void displayMemberDetails(Member member) {
+    memberID.setText(String.valueOf(member.getId()));
+    memberName.setText(member.getPerson().getName());
+    memberPhone.setText(member.getPerson().getPhoneNumber());
+    memberEmail.setText(member.getPerson().getEmail());
+  }
+
+  public void deleteMemberRecord(ActionEvent actionEvent) {
   }
 
   public void requestMenu(ContextMenuEvent contextMenuEvent) {
   }
 
-  public void fetchStudentWithKey(KeyEvent keyEvent) {
+  public void fetchMemberWithKey(KeyEvent keyEvent) {
   }
 
-  public void fetchStudentFeesDetails(MouseEvent mouseEvent) {
+  public void fetchMemberFeesDetails(MouseEvent mouseEvent) {
   }
 
   public void Close(ActionEvent actionEvent) {
@@ -68,18 +138,39 @@ public class MemberViewController {
   }
 
   public void HomeAction(ActionEvent actionEvent) {
-    
+
   }
 
   public void cancel(ActionEvent actionEvent) {
+    memberID.clear();
+    memberName.clear();
+    memberPhone.clear();
+    memberEmail.clear();
+    memberTable.getSelectionModel().clearSelection(); // Bỏ chọn trong TableView
   }
 
-  public void saveStudent(ActionEvent actionEvent) {
-  }
+  public void saveMember(ActionEvent actionEvent) {
+    // Lấy thông tin từ các TextField
+    String name = memberName.getText();
+    String phone = memberPhone.getText();
+    String email = memberEmail.getText();
 
-  public void updateStudent(ActionEvent actionEvent) {
-  }
+    // Kiểm tra xem có thành viên nào đang được chọn không
+    Member selectedMember = memberTable.getSelectionModel().getSelectedItem();
+    if (selectedMember != null) {
+      // Cập nhật thuộc tính của thành viên
+      selectedMember.getPerson().setName(name);
+      selectedMember.getPerson().setEmail(email);
+      selectedMember.getPerson().setPhoneNumber(phone);
 
-  public void deleteStudent(ActionEvent actionEvent) {
+      // Lưu thay đổi vào database nếu cần
+      //MemberDatabase.updateMember(selectedMember);
+
+      // Làm mới TableView để hiển thị thay đổi
+      memberTable.refresh();
+
+      // Xóa nội dung các TextField sau khi cập nhật
+      cancel(null);
+    }
   }
 }
