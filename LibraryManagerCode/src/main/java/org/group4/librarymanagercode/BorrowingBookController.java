274ed6d85cb0b95b3f5920a44cc66c1bb6f72034
ducahn2004 <nducanh180304@gmail.com>
2024-11-08 17:46:11 +0700

    Renew MemberDetails.fxml

---
+++ LibraryManagerCode/src/main/java/org/group4/librarymanagercode/BorrowingBookController.java
@@ -0,0 +1,29 @@
+package org.group4.librarymanagercode;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.input.KeyEvent;
+
+public class BorrowingBookController {
+
+  @FXML
+  private JFXButton submit;
+  @FXML
+  private JFXTextField lateFee;
+  @FXML
+  private ListView listView;
+
+  @FXML
+  private JFXButton renew;
+
+
+  public void submitBook(ActionEvent actionEvent) {
+  }
+
+  public void renewBook(ActionEvent actionEvent) {
+  }
+}
