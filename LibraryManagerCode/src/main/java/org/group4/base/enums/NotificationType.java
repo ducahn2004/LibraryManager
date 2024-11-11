88790d508ee16a90ff14a91be7b8e8ffbabd6595
TuanAnhSatoh <tuananhdx9a@gmail.com>
2024-11-11 21:14:32 +0700

    update code

--- LibraryManagerCode/src/main/java/org/group4/base/enums/NotificationType.java
+++ LibraryManagerCode/src/main/java/org/group4/base/enums/NotificationType.java
@@ -1,12 +1,32 @@
 package org.group4.base.enums;
 
+/**
+ * Represents the different types of notifications that can be sent.
+ * This enum is used to categorize various notifications in the system,
+ * helping to handle different scenarios, such as reminders, alerts, and updates.
+ */
 public enum NotificationType {
+  /** Notification for reminding the user of an upcoming due date for a borrowed item. */
   DUE_DATE_REMINDER,
+
+  /** Notification for informing the user that an item is overdue. */
   OVERDUE_NOTIFICATION,
+
+  /** Notification for informing the user of a fine associated with their account. */
   FINE_NOTIFICATION,
+
+  /** Notification for resetting the user's password. */
   PASSWORD_RESET,
+
+  /** Notification for informing the user about the arrival of new books in the system. */
   NEW_BOOK_ARRIVAL,
+
+  /** Notification for confirming a successful book borrowing transaction. */
   BOOK_BORROW_SUCCESS,
+
+  /** Notification for confirming a successful book return transaction. */
   BOOK_RETURN_SUCCESS,
+
+  /** General information notification for various non-critical updates. */
   INFO
 }
--- LibraryManagerCode/src/main/java/org/group4/base/notifications/Notification.java
+++ LibraryManagerCode/src/main/java/org/group4/base/notifications/Notification.java
@@ -5,13 +5,27 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import org.group4.base.enums.NotificationType;
 
+/**
+ * Represents a notification with a unique ID, creation date, type, and content.
+ * This class is designed to serve as a base for various types of notifications.
+ */
 public class Notification {
+  // Unique ID for the notification
   private final String notificationId;
+  // Date when the notification was created
   private final LocalDate createdOn;
+  // Type of notification, defined by NotificationType enum
   private final NotificationType type;
+  // Content of the notification message
   private String content;
 
-  // Constructor
+  /**
+   * Constructs a new Notification with the specified type and content.
+   * The notification ID is generated based on type and timestamp.
+   *
+   * @param type    The type of notification, specified by NotificationType enum.
+   * @param content The content or message of the notification.
+   */
   public Notification(NotificationType type, String content) {
     this.notificationId = generateNotificationId(type);
     this.createdOn = LocalDate.now();
@@ -19,28 +33,58 @@ public class Notification {
     this.content = content;
   }
 
+  /**
+   * Generates a unique notification ID by combining the type and a timestamp.
+   *
+   * @param type The type of notification.
+   * @return A unique notification ID as a String.
+   */
   private String generateNotificationId(NotificationType type) {
     return type.toString() + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
   }
 
+  /**
+   * Returns the unique ID of the notification.
+   *
+   * @return The notification ID as a String.
+   */
   public String getNotificationId() {
     return notificationId;
   }
 
+  /**
+   * Returns the date the notification was created.
+   *
+   * @return The creation date of the notification.
+   */
   public LocalDate getCreatedOn() {
     return createdOn;
   }
 
+  /**
+   * Returns the content of the notification.
+   *
+   * @return The notification content as a String.
+   */
   public String getContent() {
     return content;
   }
 
+  /**
+   * Returns the type of the notification.
+   *
+   * @return The NotificationType enum representing the type.
+   */
   public NotificationType getType() {
     return type;
   }
 
+  /**
+   * Updates the content of the notification.
+   *
+   * @param content The new content for the notification.
+   */
   public void setContent(String content) {
     this.content = content;
   }
-
 }
