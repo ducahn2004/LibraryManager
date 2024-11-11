db82c6a0e7d2aebff1531b079b60afaaac98eaa2
ducahn2004 <nducanh180304@gmail.com>
2024-11-11 11:36:05 +0700

    Update BorrowingBookController

--- LibraryManagerCode/src/main/java/org/group4/librarymanagercode/BorrowingBookController.java
+++ LibraryManagerCode/src/main/java/org/group4/librarymanagercode/BorrowingBookController.java
@@ -1,29 +1,98 @@
 package org.group4.librarymanagercode;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
+import org.group4.base.books.BookLending;
 
 public class BorrowingBookController {
 
   @FXML
-  private JFXButton submit;
+  private JFXButton homeButton;
   @FXML
-  private JFXTextField lateFee;
+  private Button submit;
   @FXML
-  private ListView listView;
-
+  private TextField lateFee;
   @FXML
-  private JFXButton renew;
+  private ListView<String> listView;
+  @FXML
+  private Button renew;
 
+  public void setBookLendingDetails(BookLending bookLending) {
+    if (bookLending != null) {
+      listView.getItems().addAll(
+          "ISBN: " + bookLending.getBookItem().getISBN(),
+          "Title: " + bookLending.getBookItem().getTitle(),
+          "Barcode: " + bookLending.getBookItem().getBarcode(),
+          "Creation Date: " + bookLending.getCreationDate().toString(),
+          "Due Date: " + bookLending.getDueDate().toString()
+      );
+    }
 
-  public void submitBook(ActionEvent actionEvent) {
+    // Set custom cell factory for horizontal layout
+    listView.setCellFactory(new Callback<>() {
+      @Override
+      public ListCell<String> call(ListView<String> listView) {
+        return new ListCell<>() {
+          @Override
+          protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item != null && !empty) {
+              HBox hbox = new HBox(10); // Spacing between elements
+              Label label = new Label(item);
+              hbox.getChildren().add(label);
+              setGraphic(hbox);
+            } else {
+              setGraphic(null);
+            }
+          }
+        };
+      }
+    });
   }
 
-  public void renewBook(ActionEvent actionEvent) {
+  @FXML
+  private void submitBook(ActionEvent actionEvent) {
+    // Handle submit event
+  }
+
+  @FXML
+  private void renewBook(ActionEvent actionEvent) {
+    // Handle renew event
+  }
+
+  @FXML
+  private void HomeAction(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  private void MemberAction(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  private void ReturnBookAction(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  private void notificationAction(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  private void Close(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  private void SettingAction(ActionEvent actionEvent) {
+  }
+
+  @FXML
+  private void BookAction(ActionEvent actionEvent) {
   }
 }
