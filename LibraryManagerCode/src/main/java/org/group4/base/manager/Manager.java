e4a5431c34f04693207fe2fb602aeb003f72d48f
TuanAnhSatoh <tuananhdx9a@gmail.com>
2024-11-15 14:26:12 +0700

    Remove base to replace module

--- LibraryManagerCode/src/main/java/org/group4/base/manager/Manager.java
+++
@@ -1,7 +0,0 @@
-package org.group4.base.manager;
-
-public interface Manager<T> {
-  boolean add(T item);
-  boolean remove(T item);
-  boolean update(T item);
-}
--- LibraryManagerCode/src/main/java/org/group4/exceptions/FinePaymentException.java
+++
@@ -1,7 +0,0 @@
-package org.group4.exceptions;
-
-public class FinePaymentException extends Exception {
-  public FinePaymentException(String message) {
-    super(message);
-  }
-}
