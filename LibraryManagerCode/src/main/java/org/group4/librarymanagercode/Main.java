435cb6bf0d425f8ce740b8618d6999195c13f810
Tranduc@1910 <22022189@vnu.edu.vn>
2024-11-16 19:10:43 +0700

    update adminPane.fxml

--- LibraryManagerCode/src/main/java/org/group4/librarymanagercode/Main.java
+++ LibraryManagerCode/src/main/java/org/group4/librarymanagercode/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
   @Override
   public void start(@NotNull Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("AdminPane.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 700, 550);
+    Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
     stage.setTitle("Library Manager");
     stage.setScene(scene);
     stage.show();
