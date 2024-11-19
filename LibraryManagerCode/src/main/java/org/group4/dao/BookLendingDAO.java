809ae535fa11f191bff857febf718d907a285ff9
TuanAnhSatoh <tuananhdx9a@gmail.com>
2024-11-19 21:54:06 +0700

    Update BookLendingDAO.java

--- LibraryManagerCode/src/main/java/org/group4/dao/BookLendingDAO.java
+++ LibraryManagerCode/src/main/java/org/group4/dao/BookLendingDAO.java
@@ -13,8 +13,8 @@ import java.util.Optional;
 
 import org.group4.module.books.BookItem;
 import org.group4.module.transactions.BookLending;
-
 import org.group4.module.users.Member;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,40 +29,50 @@ public class BookLendingDAO extends BaseDAO implements GenericDAO<BookLending, B
   /** Logger for BookLendingDAO class. */
   private static final Logger logger = LoggerFactory.getLogger(BookLendingDAO.class);
 
-  /** SQL query to add a new book lending to the database. */
+  /** Column names for the book_lendings table */
+  private static final String COLUMN_BARCODE = "barcode";
+  private static final String COLUMN_MEMBER_ID = "member_id";
+  private static final String COLUMN_LENDING_DATE = "lending_date";
+  private static final String COLUMN_DUE_DATE = "due_date";
+  private static final String COLUMN_RETURN_DATE = "return_date";
+
+  /** SQL statements for CRUD operations on the book_lendings table */
   private static final String ADD_BOOK_LENDING_SQL =
-      "INSERT INTO book_lendings (barcode, memberId, lendingDate, dueDate, returnDate) "
+      "INSERT INTO book_lendings (barcode, member_id, lending_date, due_date, return_date) "
           + "VALUES (?, ?, ?, ?, ?)";
 
-  /** SQL query to update an existing book lending in the database. */
   private static final String UPDATE_BOOK_LENDING_SQL =
-      "UPDATE book_lendings SET lendingDate = ?, dueDate = ?, returnDate = ? "
-          + "WHERE barcode = ? AND memberId = ?";
+      "UPDATE book_lendings SET lending_date = ?, due_date = ?, return_date = ? "
+          + "WHERE barcode = ? AND member_id = ?";
 
-  /** SQL query to delete a book lending from the database by barcode and member ID. */
   private static final String DELETE_BOOK_LENDING_SQL =
-      "DELETE FROM book_lendings WHERE barcode = ? AND memberId = ?";
+      "DELETE FROM book_lendings WHERE barcode = ? AND member_id = ?";
 
-  /** SQL query to find a book lending by barcode and member ID. */
   private static final String GET_BOOK_LENDING_BY_BARCODE_SQL =
       "SELECT * FROM book_lendings WHERE barcode = ?";
 
-  /** SQL query to find a book lending by member ID. */
   private static final String GET_BOOK_LENDING_BY_MEMBER_SQL =
-      "SELECT * FROM book_lendings WHERE memberId = ?";
+      "SELECT * FROM book_lendings WHERE member_id = ?";
 
-  /** SQL query to find a book lending by barcode and member ID. */
   private static final String GET_BOOK_LENDING_BY_ID_SQL =
-      "SELECT * FROM book_lendings WHERE barcode = ? AND memberId = ?";
+      "SELECT * FROM book_lendings WHERE barcode = ? AND member_id = ?";
 
-  /** SQL query to find all book lendings in the database. */
-  private static final String GET_ALL_BOOK_LENDINGS_SQL = "SELECT * FROM book_lendings";
+  private static final String GET_ALL_BOOK_LENDINGS_SQL =
+      "SELECT * FROM book_lendings";
 
   @Override
   public boolean add(BookLending bookLending) {
     try (Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(ADD_BOOK_LENDING_SQL)) {
-      setBookLendingData(preparedStatement, bookLending, false);
+      preparedStatement.setString(1, bookLending.getBookItem().getBarcode());
+      preparedStatement.setString(2, bookLending.getMember().getMemberId());
+      preparedStatement.setDate(3, Date.valueOf(bookLending.getLendingDate()));
+      preparedStatement.setDate(4, Date.valueOf(bookLending.getDueDate()));
+      if (bookLending.getReturnDate().isPresent()) {
+        preparedStatement.setDate(5, Date.valueOf(bookLending.getReturnDate().get()));
+      } else {
+        preparedStatement.setNull(5, Types.DATE);
+      }
       return preparedStatement.executeUpdate() > 0;
     } catch (SQLException e) {
       logger.error("Error adding book lending: {}", bookLending, e);
@@ -74,7 +84,15 @@ public class BookLendingDAO extends BaseDAO implements GenericDAO<BookLending, B
   public boolean update(BookLending bookLending) {
     try (Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_BOOK_LENDING_SQL)) {
-      setBookLendingData(preparedStatement, bookLending, true);
+      preparedStatement.setDate(1, Date.valueOf(bookLending.getLendingDate()));
+      preparedStatement.setDate(2, Date.valueOf(bookLending.getDueDate()));
+      if (bookLending.getReturnDate().isPresent()) {
+        preparedStatement.setDate(3, Date.valueOf(bookLending.getReturnDate().get()));
+      } else {
+        preparedStatement.setNull(3, Types.DATE);
+      }
+      preparedStatement.setString(4, bookLending.getBookItem().getBarcode());
+      preparedStatement.setString(5, bookLending.getMember().getMemberId());
       return preparedStatement.executeUpdate() > 0;
     } catch (SQLException e) {
       logger.error("Error updating book lending: {}", bookLending, e);
@@ -95,11 +113,26 @@ public class BookLendingDAO extends BaseDAO implements GenericDAO<BookLending, B
     }
   }
 
+  @Override
+  public List<BookLending> getAll() {
+    List<BookLending> bookLendings = new ArrayList<>();
+    try (Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_BOOK_LENDINGS_SQL);
+        ResultSet resultSet = preparedStatement.executeQuery()) {
+      while (resultSet.next()) {
+        bookLendings.add(mapRowToBookLending(resultSet));
+      }
+    } catch (SQLException e) {
+      logger.error("Error retrieving all book lendings", e);
+    }
+    return bookLendings;
+  }
+
   /**
-   * Retrieves a list of book lendings from the database by barcode.
+   * Retrieves all book lendings from the database by the barcode of the book item.
    *
-   * @param barcode the barcode of the book item to find lendings for
-   * @return a list of book lendings with the given barcode
+   * @param barcode The barcode of the book item to retrieve book lendings for
+   * @return A list of all book lendings for the book item with the given barcode
    */
   public List<BookLending> getByBarcode(String barcode) {
     List<BookLending> bookLendings = new ArrayList<>();
@@ -118,10 +151,10 @@ public class BookLendingDAO extends BaseDAO implements GenericDAO<BookLending, B
   }
 
   /**
-   * Retrieves a list of book lendings from the database by member ID.
+   * Retrieves all book lendings from the database by the member ID of the member.
    *
-   * @param memberId the ID of the member to find lendings for
-   * @return a list of book lendings with the given member ID
+   * @param memberId The ID of the member to retrieve book lendings for
+   * @return A list of all book lendings for the member with the given ID
    */
   public List<BookLending> getByMemberId(String memberId) {
     List<BookLending> bookLendings = new ArrayList<>();
@@ -140,11 +173,12 @@ public class BookLendingDAO extends BaseDAO implements GenericDAO<BookLending, B
   }
 
   /**
-   * Retrieves a book lending from the database by barcode and member ID.
+   * Retrieves a book lending from the database by the barcode of the book item and the member ID of
+   * the member.
    *
-   * @param barcode the barcode of the book item to find
-   * @param memberId the ID of the member to find
-   * @return an Optional containing the BookLending if found, or an empty Optional otherwise
+   * @param barcode The barcode of the book item to retrieve the book lending for
+   * @param memberId The ID of the member to retrieve the book lending for
+   * @return An Optional containing the BookLending if found, or an empty Optional otherwise
    */
   public Optional<BookLending> getById(String barcode, String memberId) {
     try (Connection connection = getConnection();
@@ -162,78 +196,25 @@ public class BookLendingDAO extends BaseDAO implements GenericDAO<BookLending, B
     return Optional.empty();
   }
 
-  @Override
-  public List<BookLending> getAll() {
-    List<BookLending> bookLendings = new ArrayList<>();
-    try (Connection connection = getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_BOOK_LENDINGS_SQL);
-        ResultSet resultSet = preparedStatement.executeQuery()) {
-      while (resultSet.next()) {
-        bookLendings.add(mapRowToBookLending(resultSet));
-      }
-    } catch (SQLException e) {
-      logger.error("Error retrieving all book lendings", e);
-    }
-    return bookLendings;
-  }
-
   /**
-   * Maps a row in a ResultSet to a BookLending object.
+   * Maps a row in the ResultSet to a BookLending object.
    *
-   * @param resultSet the ResultSet to map
-   * @return the BookLending object mapped from the ResultSet
-   * @throws SQLException if an SQL error occurs during mapping
+   * @param resultSet the ResultSet containing book lending data
+   * @return a BookLending object with data from the ResultSet
+   * @throws SQLException if a database error occurs
    */
   private BookLending mapRowToBookLending(ResultSet resultSet) throws SQLException {
-    Optional<BookItem> bookItemOpt =
-        FactoryDAO.getBookItemDAO().getById(resultSet.getString("barcode"));
-    Optional<Member> memberOpt =
-        FactoryDAO.getMemberDAO().getById(resultSet.getString("memberId"));
-
-    if (bookItemOpt.isPresent() && memberOpt.isPresent()) {
-        BookItem bookItem = bookItemOpt.get();
-        Member member = memberOpt.get();
-        LocalDate lendingDate = resultSet.getDate("lendingDate").toLocalDate();
-        LocalDate dueDate = resultSet.getDate("dueDate").toLocalDate();
-        LocalDate returnDate = resultSet.getDate("returnDate") != null ?
-            resultSet.getDate("returnDate").toLocalDate() : null;
-        return new BookLending(bookItem, member, lendingDate, dueDate, returnDate);
-    } else {
-        throw new SQLException("BookItem or Member not found for the given IDs.");
-    }
-  }
-
-  /**
-   * Sets the data for a BookLending object in a PreparedStatement.
-   *
-   * @param preparedStatement the PreparedStatement to set data in
-   * @param bookLending the BookLending object to set data from
-   * @param isUpdate true if the PreparedStatement is for an update, false if for an insert
-   * @throws SQLException if an SQL error occurs during setting data
-   */
-  private void setBookLendingData(PreparedStatement preparedStatement, BookLending bookLending,
-      boolean isUpdate) throws SQLException {
-
-    int index = 1;
-
-    if (!isUpdate) {
-      preparedStatement.setString(index++, bookLending.getBookItem().getBarcode());
-      preparedStatement.setString(index++, bookLending.getMember().getMemberId());
-    }
-    preparedStatement.setDate(index++, Date.valueOf(bookLending.getLendingDate()));
-    preparedStatement.setDate(index++, Date.valueOf(bookLending.getDueDate()));
-
-    // Set return date if present, else set to NULL
-    if (bookLending.getReturnDate().isPresent()) {
-      preparedStatement.setDate(index++, Date.valueOf(bookLending.getReturnDate().get()));
-    } else {
-      preparedStatement.setNull(index++, Types.DATE);
-    }
-
-    // Set remaining fields for update if necessary
-    if (isUpdate) {
-      preparedStatement.setString(index++, bookLending.getBookItem().getBarcode());
-      preparedStatement.setString(index, bookLending.getMember().getMemberId());
-    }
+    BookItem bookItem = FactoryDAO.getBookItemDAO()
+        .getById(resultSet.getString(COLUMN_BARCODE))
+        .orElseThrow(() -> new SQLException("BookItem not found"));
+    Member member = FactoryDAO.getMemberDAO()
+        .getById(resultSet.getString(COLUMN_MEMBER_ID))
+        .orElseThrow(() -> new SQLException("Member not found"));
+    LocalDate lendingDate = resultSet.getDate(COLUMN_LENDING_DATE).toLocalDate();
+    LocalDate dueDate = resultSet.getDate(COLUMN_DUE_DATE).toLocalDate();
+    LocalDate returnDate = resultSet.getDate(COLUMN_RETURN_DATE) != null
+        ? resultSet.getDate(COLUMN_RETURN_DATE).toLocalDate()
+        : null;
+    return new BookLending(bookItem, member, lendingDate, dueDate, returnDate);
   }
 }
