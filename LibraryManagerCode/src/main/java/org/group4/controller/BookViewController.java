bcbbab0dd09a5a73cf4ad2c1b2885bb7179f86e8
ducahn2004 <nducanh180304@gmail.com>
2024-11-22 00:14:42 +0700

    Add Comment BookViewController BorrowingBookController

--- LibraryManagerCode/src/main/java/org/group4/controller/BookViewController.java
+++ LibraryManagerCode/src/main/java/org/group4/controller/BookViewController.java
@@ -1,8 +1,11 @@
 package org.group4.controller;
 
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -23,72 +26,63 @@ import org.group4.module.books.Book;
 import org.group4.module.manager.SessionManager;
 import org.group4.module.users.Librarian;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+/**
+ * Controller for managing book-related operations in the application.
+ */
 public class BookViewController {
 
+  // FXML injected fields for UI components
   @FXML
   private JFXButton bookLendingButton;
-
   @FXML
   private JFXButton homeButton;
-
   @FXML
   private JFXButton MemberButton;
-
   @FXML
   private JFXButton bookButton;
-
   @FXML
   private JFXButton settingButton;
-
   @FXML
   private JFXButton notificationButton;
-
   @FXML
   private JFXButton closeButton;
 
   @FXML
   private TableView<Book> tableView;
-
   @FXML
   private TableColumn<Book, String> ISBN;
-
   @FXML
   private TableColumn<Book, String> bookName;
-
   @FXML
   private TableColumn<Book, String> bookSubject;
-
   @FXML
   private TableColumn<Book, String> bookPublisher;
-
   @FXML
   private TableColumn<Book, String> bookLanguage;
-
   @FXML
   private TableColumn<Book, Integer> numberOfPages;
-
   @FXML
   private TableColumn<Book, String> bookAuthor;
-
   @FXML
   private TableColumn<Book, Void> actionColumn;
 
   @FXML
   private TextField searchField;
-
   @FXML
   private Button addBookButton;
 
+  // Observable list for storing book data
   private final ObservableList<Book> bookList = FXCollections.observableArrayList();
+
+  // Current librarian retrieved from session manager
   private final Librarian librarian = SessionManager.getInstance().getCurrentLibrarian();
 
+  /**
+   * Initializes the controller and sets up the UI components and their bindings.
+   */
   @FXML
   public void initialize() {
-    // Khởi tạo các cột của bảng
+    // Configure table column bindings
     ISBN.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getISBN()));
     bookName.setCellValueFactory(
         cellData -> new SimpleStringProperty(cellData.getValue().getTitle()));
@@ -103,6 +97,7 @@ public class BookViewController {
     bookAuthor.setCellValueFactory(cellData -> new SimpleStringProperty(
         cellData.getValue().getAuthors().iterator().next().getName()));
 
+    // Configure action column with edit and delete options
     actionColumn.setCellFactory(param -> new TableCell<>() {
       private final Hyperlink editLink = new Hyperlink("Edit");
       private final Hyperlink deleteLink = new Hyperlink("Delete");
@@ -125,15 +120,10 @@ public class BookViewController {
       }
     });
 
+    // Load initial book data
     loadBookData();
-    tableView.setOnMouseClicked(event -> {
-      if (event.getClickCount() == 2) {
-        Book selectedBook = tableView.getSelectionModel().getSelectedItem();
-        if (selectedBook != null) {
-          showBookDetail(selectedBook);
-        }
-      }
-    });
+
+    // Set up search functionality
     searchField.textProperty()
         .addListener((observable, oldValue, newValue) -> filterBookList(newValue));
     searchField.setOnKeyPressed(event -> {
@@ -143,6 +133,9 @@ public class BookViewController {
     });
   }
 
+  /**
+   * Loads book data from the database and populates the table view.
+   */
   private void loadBookData() {
     List<Book> books = FactoryDAO.getBookDAO().getAll();
 
@@ -157,6 +150,11 @@ public class BookViewController {
     }
   }
 
+  /**
+   * Filters the book list based on the search text.
+   *
+   * @param searchText The text used to filter the book list.
+   */
   private void filterBookList(String searchText) {
     if (searchText == null || searchText.isEmpty()) {
       tableView.setItems(bookList);
@@ -170,6 +168,9 @@ public class BookViewController {
     }
   }
 
+  /**
+   * Refreshes the book table by reloading data from the database.
+   */
   public void refreshTable() {
     tableView.getItems().clear();
     bookList.clear();
@@ -177,9 +178,13 @@ public class BookViewController {
     tableView.setItems(bookList);
   }
 
+  /**
+   * Displays the details of a selected book in a new window.
+   *
+   * @param book The book whose details are to be displayed.
+   */
   private void showBookDetail(Book book) {
     try {
-      // Tạo FXMLLoader và mở cửa sổ chi tiết sách
       FXMLLoader loader = new FXMLLoader(getClass().getResource("BookDetails.fxml"));
       Parent root = loader.load();
       Stage stage = new Stage();
@@ -190,42 +195,67 @@ public class BookViewController {
 
       stage.setTitle("Book Detail");
       stage.show();
-    } catch (IOException e) {
+    } catch (IOException | SQLException e) {
       logAndShowError("Failed to load book detail form", e);
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
     }
   }
 
+  /**
+   * Logs an error and displays an alert to the user.
+   *
+   * @param message The error message.
+   * @param e       The exception that occurred.
+   */
+  private void logAndShowError(String message, Exception e) {
+    Logger.getLogger(BookViewController.class.getName()).log(Level.SEVERE, message, e);
+    showAlert(AlertType.ERROR, "Error", message);
+  }
+
+  /**
+   * Displays the edit form for the specified book.
+   *
+   * @param book The book to be edited.
+   */
   private void showEditForm(Book book) {
     try {
       FXMLLoader loader = new FXMLLoader(getClass().getResource("EditBook.fxml"));
       Stage editStage = new Stage();
       editStage.setScene(new Scene(loader.load()));
 
+      // Pass book data and parent controller to the edit form
       EditBookController controller = loader.getController();
       controller.setBookData(book);
       controller.setParentController(this);
 
-      editStage.setTitle("Edit Member");
+      editStage.setTitle("Edit Book");
       editStage.showAndWait();
     } catch (IOException e) {
       logAndShowError("Failed to load edit form page", e);
     }
   }
 
+  /**
+   * Displays a confirmation dialog for deleting the specified book.
+   *
+   * @param book The book to be deleted.
+   */
   private void showDeleteConfirmation(Book book) {
-    Alert alert = createAlert(AlertType.CONFIRMATION, "Delete Confirmation",
+    Alert alert = createAlert(
+        AlertType.CONFIRMATION,
+        "Delete Confirmation",
         "Are you sure you want to delete this book?",
-        "ID: " + book.getISBN() + "\nName: " + book.getTitle() + "\nSubject: " + book.getSubject()
-            + "\nLanguage: " + book.getLanguage() + "\nNumber Of Pages: " + book.getNumberOfPages()
-            + "\nAuthor: " + book.authorsToString());
+        "ID: " + book.getISBN()
+            + "\nName: " + book.getTitle()
+            + "\nSubject: " + book.getSubject()
+            + "\nLanguage: " + book.getLanguage()
+            + "\nNumber Of Pages: " + book.getNumberOfPages()
+            + "\nAuthor: " + book.authorsToString()
+    );
 
     alert.showAndWait().ifPresent(response -> {
       if (response == ButtonType.OK) {
+        // Delete the book and update the table view
         if (librarian.deleteBook(book.getISBN())) {
-          // TODO Uncomment after notification complete
-          //SystemNotification.sendNotification(String type, String content);
           System.out.println(
               "Book with ISBN: " + book.getISBN() + " has been deleted successfully.");
           bookList.remove(book);
@@ -233,23 +263,17 @@ public class BookViewController {
           System.out.println("Failed to delete book with ISBN: " + book.getISBN());
           showAlert(AlertType.ERROR, "Deletion Failed",
               "The book with the ISBN " + book.getISBN()
-                  + " could not be deleted because it already exists in the library.");
+                  + " could not be deleted because it is still in use.");
         }
       }
     });
   }
 
-
-  private void logAndShowError(String message, Exception e) {
-    Logger.getLogger(BookViewController.class.getName()).log(Level.SEVERE, message, e);
-    showAlert(AlertType.ERROR, "Error", message);
-  }
-
   /**
-   * A single method for handling various alert types.
+   * Displays an alert with the specified type, title, and content.
    *
-   * @param type    The type of the alert.
-   * @param title   The title of the alert.
+   * @param type    The type of alert to display.
+   * @param title   The title of the alert dialog.
    * @param content The content message of the alert.
    */
   void showAlert(AlertType type, String title, String content) {
@@ -260,6 +284,15 @@ public class BookViewController {
     alert.showAndWait();
   }
 
+  /**
+   * Creates a new alert dialog with the specified parameters.
+   *
+   * @param type    The type of alert.
+   * @param title   The title of the alert.
+   * @param header  The header text of the alert.
+   * @param content The content message of the alert.
+   * @return The created Alert object.
+   */
   private Alert createAlert(AlertType type, String title, String header, String content) {
     Alert alert = new Alert(type);
     alert.setTitle(title);
@@ -268,6 +301,12 @@ public class BookViewController {
     return alert;
   }
 
+  /**
+   * Handles the action for adding a new book. Switches to the "AddBook.fxml" scene.
+   *
+   * @param actionEvent The event triggered by the "Add Book" button.
+   * @throws IOException If the FXML file cannot be loaded.
+   */
   @FXML
   public void addBookAction(ActionEvent actionEvent) throws IOException {
     FXMLLoader loader = new FXMLLoader(getClass().getResource("AddBook.fxml"));
@@ -276,44 +315,87 @@ public class BookViewController {
     stage.getScene().setRoot(root);
   }
 
+  /**
+   * Handles the action for navigating to the home view.
+   *
+   * @param actionEvent The event triggered by the "Home" button.
+   * @throws IOException If the FXML file cannot be loaded.
+   */
   @FXML
   public void HomeAction(ActionEvent actionEvent) throws IOException {
     SceneSwitcher.switchScene(getStage(), "AdminPane.fxml", "Library Manager");
   }
 
+  /**
+   * Handles the action for navigating to the member view.
+   *
+   * @param actionEvent The event triggered by the "Member" button.
+   * @throws IOException If the FXML file cannot be loaded.
+   */
   @FXML
   public void MemberAction(ActionEvent actionEvent) throws IOException {
     SceneSwitcher.switchScene(getStage(), "MemberView.fxml", "Library Manager");
   }
 
+  /**
+   * Handles the action for navigating to the book view.
+   *
+   * @param actionEvent The event triggered by the "Book" button.
+   * @throws IOException If the FXML file cannot be loaded.
+   */
   @FXML
   public void BookAction(ActionEvent actionEvent) throws IOException {
     SceneSwitcher.switchScene(getStage(), "BookView.fxml", "Library Manager");
   }
 
+  /**
+   * Handles the action for navigating to the notification view.
+   *
+   * @param actionEvent The event triggered by the "Notification" button.
+   * @throws IOException If the FXML file cannot be loaded.
+   */
   @FXML
   public void notificationAction(ActionEvent actionEvent) throws IOException {
     SceneSwitcher.switchScene(getStage(), "Notification.fxml", "Library Manager");
   }
 
+  /**
+   * Handles the action for navigating to the settings view.
+   *
+   * @param actionEvent The event triggered by the "Setting" button.
+   * @throws IOException If the FXML file cannot be loaded.
+   */
   @FXML
   public void SettingAction(ActionEvent actionEvent) throws IOException {
     SceneSwitcher.switchScene(getStage(), "Setting.fxml", "Library Manager");
   }
 
+  /**
+   * Handles the action for navigating to the book lending view.
+   *
+   * @param actionEvent The event triggered by the "Book Lending" button.
+   * @throws IOException If the FXML file cannot be loaded.
+   */
   public void BookLendingAction(ActionEvent actionEvent) throws IOException {
     SceneSwitcher.switchScene(getStage(), "BookLending.fxml", "Library Manager");
   }
 
-
+  /**
+   * Exits the application.
+   *
+   * @param actionEvent The event triggered by the "Close" button.
+   */
   @FXML
   public void Close(ActionEvent actionEvent) {
     Platform.exit();
   }
 
+  /**
+   * Retrieves the current stage.
+   *
+   * @return The current stage.
+   */
   private Stage getStage() {
     return (Stage) homeButton.getScene().getWindow();
   }
-
-
 }
--- LibraryManagerCode/src/main/java/org/group4/controller/BorrowingBookController.java
+++ LibraryManagerCode/src/main/java/org/group4/controller/BorrowingBookController.java
@@ -15,56 +15,93 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.group4.dao.FactoryDAO;
 import org.group4.module.books.BookItem;
-
+import org.group4.module.enums.BookStatus;
 import org.group4.module.manager.SessionManager;
 import org.group4.module.transactions.BookLending;
-import org.group4.module.enums.BookStatus;
 import org.group4.module.users.Librarian;
 import org.group4.module.users.Member;
 
+/**
+ * Controller for managing the borrowing of books. Handles interactions between the user interface
+ * and the business logic for borrowing books in the library system.
+ */
 public class BorrowingBookController {
 
+  /**
+   * The librarian currently managing the session.
+   */
   private final Librarian librarian = SessionManager.getInstance().getCurrentLibrarian();
+
   @FXML
   private Button cancelButton;
+
   @FXML
   private Label priceField;
+
   @FXML
   private TextField memberIdField;
+
   @FXML
   private TextField memberNameField;
+
   @FXML
   private DatePicker dobDatePicker;
+
   @FXML
   private TextField emailField;
+
   @FXML
   private TextField phoneField;
+
   @FXML
   private Label barcodeField;
+
   @FXML
   private Label placeField;
+
   @FXML
   private Label subjectField;
+
   @FXML
   private Label languageField;
+
   @FXML
   private Label authorField;
+
   @FXML
   private Label noPageField;
+
   @FXML
   private Label isbnField;
+
   @FXML
   private Label titleField;
+
   @FXML
   private Label referenceOnlyCheck;
 
+  /**
+   * The book item currently being borrowed.
+   */
   private BookItem currentBookItem;
+
+  /**
+   * The current borrowing transaction.
+   */
   private BookLending currentBookLending;
 
+  /**
+   * Returns the current book lending transaction.
+   *
+   * @return The current {@code BookLending} object.
+   */
   public BookLending returnBookLending() {
     return this.currentBookLending;
   }
 
+  /**
+   * Initializes the controller. Sets up listeners and prepares the user interface.
+   */
   @FXML
   private void initialize() {
     // Add a listener to the memberIdField to trigger search on input
@@ -75,6 +112,11 @@ public class BorrowingBookController {
     });
   }
 
+  /**
+   * Searches for a member by ID and updates the UI with the member's details.
+   *
+   * @param memberId The ID of the member to find.
+   */
   private void findMemberById(String memberId) {
     Member foundMember = null;
     for (Member member : FactoryDAO.getMemberDAO().getAll()) {
@@ -96,6 +138,12 @@ public class BorrowingBookController {
     }
   }
 
+  /**
+   * Retrieves a member by their ID.
+   *
+   * @param memberId The ID of the member.
+   * @return The {@code Member} object if found, otherwise null.
+   */
   private Member returnMember(String memberId) {
     Member foundMember = null;
     for (Member member : FactoryDAO.getMemberDAO().getAll()) {
@@ -107,6 +155,11 @@ public class BorrowingBookController {
     return foundMember;
   }
 
+  /**
+   * Sets the details of the book to be borrowed in the UI.
+   *
+   * @param bookItem The book item to display.
+   */
   public void setItemDetailBorrowing(BookItem bookItem) {
     this.currentBookItem = bookItem;
     isbnField.setText(bookItem.getISBN());
@@ -115,13 +168,19 @@ public class BorrowingBookController {
     languageField.setText(bookItem.getLanguage());
     authorField.setText(bookItem.authorsToString());
     noPageField.setText(String.valueOf(bookItem.getNumberOfPages()));
-
     barcodeField.setText(bookItem.getBarcode());
     placeField.setText(bookItem.getPlacedAt().getLocationIdentifier());
     priceField.setText(Double.toString(bookItem.getPrice()));
     referenceOnlyCheck.setText((bookItem.getIsReferenceOnly()) ? "Yes" : "No");
   }
 
+  /**
+   * Handles the borrowing process for a book.
+   *
+   * @param bookItem The book item to be borrowed.
+   * @param member   The member borrowing the book.
+   * @throws Exception If the borrowing process fails.
+   */
   private void borrowingBook(BookItem bookItem, Member member) throws Exception {
     boolean isBorrowed = librarian.borrowBookItem(bookItem, member);
     if (!isBorrowed) {
@@ -134,8 +193,13 @@ public class BorrowingBookController {
     }
   }
 
+  /**
+   * Handles the submission of the borrowing form.
+   *
+   * @param actionEvent The event triggered by clicking the submit button.
+   * @throws Exception If the submission process fails.
+   */
   public void handleSubmit(ActionEvent actionEvent) throws Exception {
-
     if (memberNameField.getText().isEmpty() || memberIdField.getText().isEmpty() ||
         emailField.getText().isEmpty() || phoneField.getText().isEmpty()) {
 
@@ -151,10 +215,6 @@ public class BorrowingBookController {
             returnMember(memberIdField.getText()));
         borrowingBook(currentBookLending.getBookItem(), currentBookLending.getMember());
 
-        System.out.println(
-            "MEMBER ID: " + currentBookLending.getMember().getMemberId() + " BORROWED");
-        System.out.println("BarCode: " + currentBookLending.getBookItem().getBarcode() + " "
-            + "BORROWED");
         Alert alert = new Alert(AlertType.INFORMATION);
         alert.setTitle("Success");
         alert.setHeaderText(null);
@@ -166,23 +226,30 @@ public class BorrowingBookController {
     }
   }
 
+  /**
+   * Cancels the borrowing process and reloads the book details view.
+   *
+   * @param actionEvent The event triggered by clicking the cancel button.
+   * @throws IOException  If loading the book details view fails.
+   * @throws SQLException If a database error occurs.
+   */
   public void handleCancel(ActionEvent actionEvent) throws IOException, SQLException {
     loadBookDetail();
   }
 
+  /**
+   * Loads the book details view and updates the UI.
+   *
+   * @throws IOException  If loading the view fails.
+   * @throws SQLException If a database error occurs.
+   */
   private void loadBookDetail() throws IOException, SQLException {
     FXMLLoader loader = new FXMLLoader(getClass().getResource("BookDetails.fxml"));
     Scene bookDetailScene = new Scene(loader.load());
-
     Stage currentStage = (Stage) memberIdField.getScene().getWindow();
-
     currentStage.setScene(bookDetailScene);
 
     BookDetailsController bookDetailsController = loader.getController();
     bookDetailsController.setItemDetail(currentBookItem);
   }
-
-
 }
-
-
